5b42573e1d037cdec0496781826384d1823267a5
nolovenopain <chienthanga2.ftu@gmail.com>
2024-01-10 15:33:55 +0700

    update

--- src/Navigations/rootNavigations.tsx
+++ src/Navigations/rootNavigations.tsx
@@ -1,41 +1,60 @@
-import {createNavigationContainerRef} from '@react-navigation/native';
+import {createRef} from 'react';
+import {CommonActions, DrawerActions, NavigationContainerRef} from '@react-navigation/native';
 import {StackActions} from '@react-navigation/native';
 
-export const navigationRef =
-  createNavigationContainerRef<ReactNavigation.RootParamList>();
+export const navigationRef = createRef<NavigationContainerRef<any>>();
 
-export const navigate = (screen: never, params: never) => {
-  if (navigationRef.isReady()) {
-    navigationRef.current?.navigate(screen, params);
-  }
+export const navigate = (screen: string, params: any) => {
+  navigationRef.current?.navigate(screen, params);
 };
 
 export const goBack = () => {
-  if (navigationRef.isReady()) {
-    navigationRef.current?.goBack();
-  }
+  navigationRef.current?.goBack();
+};
+
+export const route = () => {
+  navigationRef.current?.getCurrentRoute();
 };
 
 export const replace = (name: string, params?: object) => {
-  if (navigationRef.isReady()) {
-    navigationRef.dispatch(StackActions.replace(name, params));
-  }
+  navigationRef.current?.dispatch(StackActions.replace(name, params));
+};
+
+export const reset = (name: string, params?: object) => {
+  navigationRef.current?.dispatch(
+    CommonActions.reset({
+      index: 0,
+      routes: [
+        {
+          name,
+          params,
+        },
+      ],
+    }),
+  );
 };
 
 export const push = (name: string, params?: object) => {
-  if (navigationRef.isReady()) {
-    navigationRef.dispatch(StackActions.push(name, params));
-  }
+  navigationRef.current?.dispatch(StackActions.push(name, params));
 };
 
 export const pop = (count: number = 1) => {
-  if (navigationRef.isReady()) {
-    navigationRef.dispatch(StackActions.pop(count));
-  }
+  navigationRef.current?.dispatch(StackActions.pop(count));
 };
 
 export const popToTop = () => {
-  if (navigationRef.isReady()) {
-    navigationRef.dispatch(StackActions.popToTop());
-  }
+  navigationRef.current?.dispatch(StackActions.popToTop());
 };
+
+export const resetTo = (routes: Array<{ name: string; params?: any }>, index = 0) => {
+  navigationRef.current?.dispatch(
+    CommonActions.reset({
+      routes,
+      index,
+    }),
+  )
+};
+
+export const toggleDrawer = () => {
+  navigationRef.current?.dispatch(DrawerActions.toggleDrawer());
+}
